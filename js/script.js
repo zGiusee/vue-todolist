ba87711c5cf88b221311b85f1fdf5e7007e945e0
zGiusee <giusebelforte@gmail.com>
2023-12-13 15:12:59 +0100

    [UPDATE] used dom for-cycle to the todoList array

--- js/script.js
+++ js/script.js
@@ -5,6 +5,32 @@ createApp({
     data(){
         return {
 
+            todoList: [
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: "Finire l'esecizio di Mercoledì scorso",
+                    done: false,
+                },
+                {
+                    text: 'Pagare rata della macchina',
+                    done: false,
+                },
+                {
+                    text: 'Comprare i croccantini al cane',
+                    done: false,
+                },
+                {
+                    text: 'Accompagnare mia sorella a fare shopping',
+                    done: false,
+                },
+                {
+                    text: 'Portare i regali di natale a casa di Zio Antonio',
+                    done: false,
+                },
+            ],
         }
     },
     created() {
