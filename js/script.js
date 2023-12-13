238c6ab86062c3df1ec63e5b2e559976a75b17e0
zGiusee <giusebelforte@gmail.com>
2023-12-13 14:28:16 +0100

    [UPDATE] scaffolding done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Inizializzo l'applicazione
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return {
+
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        
+    },
+}).mount('#app')
